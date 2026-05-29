afdec5f9e2abe95f955a223bf10ee05470a44b0a
msjo1 <50199526+msjo1@users.noreply.github.com>
2026-05-29 21:50:45 +0100

    Update main.py

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ def get_plastic():
     cur.execute("""
         SELECT id,
         ST_AsGeoJSON(geom)
-        FROM plastic_detection
+        FROM Plastic_Detection_DB
     """)
 
     rows = cur.fetchall()
