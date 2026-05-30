9b017d7e630a67469bf79a75dc1391cbda3f2f6d
msjo1 <50199526+msjo1@users.noreply.github.com>
2026-05-30 15:47:08 +0100

    Update app.js

--- frontend/app.js
+++ frontend/app.js
@@ -1,31 +1,11 @@
 alert("app.js loaded");
 
-var map = L.map('map').setView([9, 8], 5);
-
-L.tileLayer(
-'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
-{
-    attribution: '© OpenStreetMap'
-}
-).addTo(map);
-
 fetch("https://plasticportal.onrender.com/plastic")
 .then(response => response.json())
 .then(data => {
 
-    data.forEach(item => {
+    alert("Data received");
 
-        var geom = JSON.parse(item.geometry);
-
-        var coords = geom.coordinates;
-
-        L.marker([
-            coords[1],   // latitude
-            coords[0]    // longitude
-        ])
-        .addTo(map)
-        .bindPopup("Plastic Detection ID: " + item.id);
-
-    });
+    console.log(data);
 
 });
