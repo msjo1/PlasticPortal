4178db53824347dff9d47ea435d0f5c172b0c820
msjo1 <50199526+msjo1@users.noreply.github.com>
2026-05-30 16:26:04 +0100

    Update app.js

--- frontend/app.js
+++ frontend/app.js
@@ -1,3 +1,12 @@
+var map = L.map('map').setView([9, 8], 5);
+
+L.tileLayer(
+'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+{
+    attribution: '© OpenStreetMap'
+}
+).addTo(map);
+
 fetch("https://plasticportal.onrender.com/plastic")
 .then(response => response.json())
 .then(data => {
