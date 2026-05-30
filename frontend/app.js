ca1f05897051655020dcd781b81db03762324e8f
msjo1 <50199526+msjo1@users.noreply.github.com>
2026-05-30 16:19:40 +0100

    Update app.js

--- frontend/app.js
+++ frontend/app.js
@@ -1,28 +1,19 @@
-var map = L.map('map').setView([9, 8], 5);
-
-L.tileLayer(
-'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
-{
-    attribution: '© OpenStreetMap'
-}
-).addTo(map);
-
 fetch("https://plasticportal.onrender.com/plastic")
 .then(response => response.json())
 .then(data => {
+
+    console.log("API DATA:", data);
 
     data.forEach(item => {
 
         var geom = JSON.parse(item.geometry);
 
-        var coords = geom.coordinates;
+        L.geoJSON(geom)
+        .bindPopup(
+            "Plastic Detection ID: " + item.id
+        )
+        .addTo(map);
 
-        L.marker([
-            coords[1],   // latitude
-            coords[0]    // longitude
-        ])
-        .addTo(map)
-        .bindPopup("Plastic Detection ID: " + item.id);
-});
+    });
 
 });
