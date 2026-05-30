b147f85a630658c79db8c97ff063593e6bccd5b0
msjo1 <50199526+msjo1@users.noreply.github.com>
2026-05-30 20:24:43 +0100

    Update main.py

--- main.py
+++ main.py
@@ -35,6 +35,29 @@ def country_summary():
 
     return rows
 
+@app.get("/plastic")
+def get_plastic():
+
+    cur = conn.cursor()
+
+    cur.execute("""
+        SELECT id,
+        ST_AsGeoJSON(geom)
+        FROM plastic_detection
+    """)
+
+    rows = cur.fetchall()
+
+    output = []
+
+    for row in rows:
+
+        output.append({
+            "id": row[0],
+            "geometry": row[1]
+        })
+
+    return output
 
 
 
